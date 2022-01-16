18e1d723d88fe1a08c36551b5dd394630fb21c47
Pau Villaverde Magrí <pauvima15@gmail.com>
2022-01-16 13:34:39 +0100

    Add files via upload

--- MyClientFrame.java
+++ MyClientFrame.java
@@ -9,13 +9,16 @@ import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
-import javax.swing.BoxLayout;
+import javax.swing.SwingUtilities;
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.event.ActionListener;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyAdapter;
+import java.io.IOException;
 
 public class MyClientFrame extends JFrame implements ActionListener {
 
@@ -27,12 +30,20 @@ public class MyClientFrame extends JFrame implements ActionListener {
     private JScrollPane messageArea;
     private JScrollPane userArea;
     private JTextArea messages;
+    private String line;
+    private MySocket sc;
 
-    public MyClientFrame() {
+    private int key;
+
+    public MyClientFrame(String adress, int port) {
 
         setTitle("Chat");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setFocusable(true);
         setResizable(false);
+        //addKeyListener(new Key());
+
+        sc = new MySocket(adress, port);
 
         chatPane = new JPanel();
         writePane = new JPanel();
@@ -44,6 +55,8 @@ public class MyClientFrame extends JFrame implements ActionListener {
 
         usersList = new JList();
         messages = new JTextArea(26,47);
+        messages.setFont(messages.getFont().deriveFont(12f));
+        messages.setEditable(false);
 
         userArea = new JScrollPane(usersList);
         messageArea = new JScrollPane(messages);
@@ -56,11 +69,11 @@ public class MyClientFrame extends JFrame implements ActionListener {
         writeArea = new JTextField();
         button = new JButton("Send");
 
+        button.addActionListener(this);
+
         writePane.add(writeArea, Component.CENTER_ALIGNMENT);
         writePane.add(Box.createRigidArea(new Dimension(5,0)));
         writePane.add(button, Component.CENTER_ALIGNMENT);
-
-        //buttonPane.setLayout(new BoxLayout(buttonPane, BoxLayout.LINE_AXIS));
 
         Container contentPane = getContentPane();
         contentPane.add(chatPane, BorderLayout.CENTER);
@@ -71,7 +84,43 @@ public class MyClientFrame extends JFrame implements ActionListener {
     }
 
     public void actionPerformed(ActionEvent e) {
+        if (key == KeyEvent.VK_ENTER || e.getSource() == button) {
+            try {
+                if((line = writeArea.getText()) != null) {
+                    sc.println(line);
+                    messages.setText(messages.getText() + "\n" + line);
+                    writeArea.setText("");
+                }
+            } catch(IOException ex) {
+                ex.printStackTrace();
+            }
+        }
 
+        new Thread(new Runnable() {
+            public void run() {
+
+                while((line = sc.readLine()) != null) {
+                    SwingUtilities.invokeLater(new Runnable() {
+                        public void run() {
+                            messages.setText(messages.getText() + "\n" + line);
+                        }
+                    });
+                }
+            }
+        }).start();
     }
+
+    /*public class Key extends KeyAdapter {
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+            key = e.getKeyCode();
+
+            if (key == KeyEvent.VK_ENTER) {
+                System.out.println("hola");
+            }
+        }
+    }*/
 
 }
